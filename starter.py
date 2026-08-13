ded9f90edc2445eba59924d960109414648f235d
Petr Urban <petr.urban447@gmail.com>
2026-08-13 13:46:32 +0200

    aaa

--- starter.py
+++ starter.py
@@ -223,7 +223,7 @@ def main() -> int:
     config = load_config()
 
     try:
-        api_key = "VfVtU_Zf-G8KnHEgSdIzFQZXLJmlR0NGLPsWxcBFaLY"
+        api_key = "TODO"
     except KeyError:
         raise RuntimeError(
             "Missing required configuration key: api_key_red"
