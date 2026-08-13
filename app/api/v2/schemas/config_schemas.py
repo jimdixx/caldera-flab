8d3a6949c62382b0f00e4fb83aff595e7985c967
Petr Urban <petr.urban447@gmail.com>
2026-08-13 09:34:26 +0200

    Init

---
+++ app/api/v2/schemas/config_schemas.py
@@ -0,0 +1,20 @@
+import marshmallow as ma
+from marshmallow import fields
+
+
+class ConfigUpdateItemSchema(ma.Schema):
+    prop = fields.String(required=True)
+    value = fields.String(required=True)
+
+class ConfigUpdateSchema(ma.Schema):
+    updates = fields.List(fields.Nested(ConfigUpdateItemSchema), required=True)
+
+class AgentConfigUpdateSchema(ma.Schema):
+    sleep_min = fields.Integer()
+    sleep_max = fields.Integer()
+    watchdog = fields.Integer()
+    untrusted_timer = fields.Integer()
+    implant_name = fields.String()
+    bootstrap_abilities = fields.List(fields.String)
+    deadman_abilities = fields.List(fields.String)
+    deployments = fields.List(fields.String, dump_only=True)
